29512a2df82a683c346563f0e186b12253ac72c3
Tomo Sindberg <tsindberg@gmail.com>
2015-06-29 13:44:28 -0700

    all done

--- library.js
+++ library.js
@@ -1,24 +1,35 @@
 
 //  Model a library using objects.
 //  A library contains shelves.
-//  Shelves contain books.############
-//  Books have an author and a title.//////
-//  Give the library methods to add and remove a shelf.///
-//  A shelf should have methods to add/remove a book./////
-//  Use a modular approach so that each type of object///
+//  Shelves contain books.
+//  Books have an author and a title.
+//  Give the library methods to add and remove a shelf.
+//  A shelf should have methods to add/remove a book.
+//  Use a modular approach so that each type of object
 //   is contained in its own file.
 //  Add any functionality you think appropriate.
 /////////////////////////////////////////////
+var shelf = [],
+    list =[],
+    booksOnShelf= [],
+    bookT = [],
+    bookA = [];
+
 var Library = {
-  removeShelf: function(shelfNumber) {
-    var i = shelf.length - 1;
-    shelf.pop({shelf: i});
-    console.log("removed shelf #" + i);
+  removeShelf: function(position, remove) {
+    //var i = shelf.length - 1;
+    //shelf.pop({shelf: i});
+    shelf.splice(position, remove);
   },
-  addShelf: function() {
+  addShelf: function(add) {
     var i = shelf.length;
+    for(j=0; j<add; j++) {
     shelf.push({shelf: i});
-    console.log("Added shelf #" + i);
+    i++;
+    }
+  },
+  listShelf: function() {
+    console.log(shelf);
   }
 };
 
@@ -31,16 +42,16 @@ var Shelf = {
   var t = bookT.indexOf(title);
   console.log(t);
     if (bookT[t]=== title) {
-      console.log(bookT[t] + "  " + bookA[t] + " has been removed. ");
+      list = ["removed" + " " + (bookT[t])];
       bookT.splice(t,1);
       bookA.splice(t,1);
     }
-    if (t == -1){
+    if (t === -1){
     console.log("Sorry, I dont have " + title);
     }
+    //this.listBooks();
   },
-
-  addBooksToShelf: function() {
+  listBooks: function() {
     booksPerShelf = 2;
     var i = 0;
     for (x=0; x<shelf.length; x++) {
@@ -49,129 +60,40 @@ var Shelf = {
           break;
         }
         booksOnShelf[x] ="Shelf "+ x + " Title: " +
-         bookT[i]; // + "   Author: " + bookA[i];
-        console.log(booksOnShelf[x]);
+         bookT[i] + "   Author: " + bookA[i];
+        list.push(booksOnShelf[x]);
         i++;
       }
+    }console.log(list);
+    if (bookT.length > (booksPerShelf * shelf.length)){
+      console.log("Too many books, need more shelfs!!");
     }
-  },
-
-  listBooks: function() {
-    for (i=0; i<bookT.length; i++) {
-      console.log("Title: " + bookT[i] +
-        "   Author: " + bookA[i]);
-      //console.log("book  " + i);
+    if (bookT.length == (booksPerShelf * shelf.length)) {
+      console.log("Shelfs are full buy another shelf before buying more books");
     }
   }
 };
-var shelf = [],
-    booksOnShelf= [],
-    bookT = [],
-    bookA = [];
 
-// shelf.push({shelf: 0});
-// shelf.push({shelf: 1});
-Library.addShelf();
-Library.addShelf();
-Library.addShelf();
-Library.addShelf();
-Library.addShelf();
-Library.addShelf();
-Library.removeShelf();
+Library.addShelf(5);
 console.log(shelf);
+Library.removeShelf(1,2);
+console.log(shelf);
+Library.listShelf();
+Shelf.listBooks();
 
+// Shelf.addBook("In Search of Lost Time", "Marcel Proust");
+// Shelf.addBook("Ulyssess", "James Joyce");
+// Shelf.addBook("Moby Dick", "Herman Melville");
+// Shelf.addBook("Hamlet", "William Shakespeare");
+// Shelf.addBook("War and Peace", "Leo Tolstoy");
+// Shelf.addBook("The Odyssey", "Homer");
+// Shelf.addBook("The Divine Comedy", "Dante Alighieri");
+// Shelf.addBook("The Great Gatsby", "F. Scott Fitzgerald");
+// Shelf.addBook("Madame Bovary", "Gustave Flaubert");
+// Shelf.listBooks();
+// // //console.log(list);
 
-Shelf.addBook("In Search of Lost Time", "Marcel Proust");
-Shelf.addBook("Ulyssess", "James Joyce");
-Shelf.addBook("Moby Dick", "Herman Melville");
-Shelf.addBook("Hamlet", "William Shakespeare");
-Shelf.addBook("War and Peace", "Leo Tolstoy");
-Shelf.addBook("The Odyssey", "Homer");
-Shelf.addBook("The Divine Comedy", "Dante Alighieri");
-Shelf.addBook("The Great Gatsby", "F. Scott Fitzgerald");
-Shelf.addBook("Madame Bovary", "Gustave Flaubert");
-//Shelf.listBooks();
-Shelf.addBooksToShelf();
-
-// var test = ["apple", "naner", "Cherry"];
-// var t = test.indexOf("Cherry");
-// console.log(t);
-
-Shelf.removeBook("Moby Dick");
-Shelf.addBooksToShelf();
-//var t = book.indexOf('Ulyssess');
-//console.log(t);
-// var x = book.join();
-// console.log(x);
-
-  // removeShelf: function(shelfNumber) {
-  //   var i = shelf.length;
-  //   shelf.pop({shelf: i});
-  // },
-  // this.addBook = function(title, author){
-  //   var listbooks= [];
-
-  //   listbooks = $.map(Book, function(value, index) {
-  //     return [value];
-
-  //   });
-  // };
-  //   for (i=0, i<Book.length)
-  //   find last book then add another number
-  //   book[i] = new Book(title, author);
-  // }
-//   this.removeBook = function(book){
-// find book by author find Book[i]
-//     then remove book
-  //    delete Book[i]
-//   }
-
-
-
-//Shelf.addBook("hello", "worldAuthor");
-//console.log(book11);
-
-// function Book(title, author)  {
-//   this.title = title;
-//   this.author = author;
-//   // this.sort = function() {
-//   //     console.log(this.author);
-//   // };
-// }
-//console.log(Book.value);
-
-//console.log(books[0]);
-//console.log(books[1]);
-//console.log(shelf3);
-
-//var swLibrary = new Library("sw");
-// console.log("shelf Length" + shelf.length);
-// Library.addShelf();
-// console.log("shelf Length" + shelf.length);
-// Library.addShelf();
-// console.log("shelf Lenght" + shelf.length);
-// Library.removeShelf();
-// console.log("shelf Lenght" + shelf.length);
-// Library.removeShelf();
-// console.log("shelf Lenght" + shelf.length);
-// Library.addShelf();
-// console.log("shelf Lenght" + shelf.length);
-// Library.addShelf();
-// console.log("shelf Lenght" + shelf.length);
-// Library.addShelf();
-// console.log("shelf Lenght" + shelf.length);
-// Library.removeShelf();
-// console.log("shelf Lenght" + shelf.length);
-
-//console.log(Library.removeShelf(2));
-//console.log(Library.addShelf(2));
-
-//console.log(shelf[3]);
-// shelf1[1] =  shelf:1;
-// var shelf2 = new Shelf(2);
-// var shelf3 = new Shelf(3);
- // this.shelfNumber = shelfNumber;
-  // removeBook: function (title){
-  //   //find book title
-  //   //then remove
-  // }
+// // Shelf.listBooks();
+// Shelf.removeBook("Moby Dick");
+// // //Shelf.addBooksToShelf(2);
+// Shelf.listBooks();
