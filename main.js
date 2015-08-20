18e1fdf9085246ce2a8e073439d811a3eea88016
Tomo Sindberg <tsindberg@gmail.com>
2015-08-19 21:13:38 -0700

    Finished

--- main.js
+++ main.js
@@ -30,44 +30,24 @@ library.addShelf(6);
 library.addShelf(4);
 library.addShelf(5);
 
-
-// console.log(library);
-// console.log(library.string());  //library string populates with shelf num
-
-// console.log(LObject.shelf[0]);
-// library.removeShelf(3);//removed shelf from string and shelves[].
-//****** LObject.shelf[3].addBook("War and Peace", "Leo Tolstoy");
-//*****runs after removeShelf3 crashes removeShelf3 is not defined
-
-
-// console.log(library);
-// console.log(library.string());  // console.log(LObject.lshelf[1]);
-
-// console.log(library.string());
-// console.log(library);  //*****
+//adding books to shelfs
 LObject.shelf[0].addBook("In Search of Lost Time", "Marcel Proust");
 LObject.shelf[0].addBook("Ulyssess", "James Joyce");
 LObject.shelf[1].addBook("Moby Dick", "Herman Melville");
 LObject.shelf[2].addBook("Hamlet", "William Shakespeare");
 LObject.shelf[3].addBook("War and Peace", "Leo Tolstoy");
-// LObject.shelf[3].addBook("The Odyssey", "Homer");
+LObject.shelf[3].addBook("The Odyssey", "Homer");
 LObject.shelf[5].addBook("The Divine Comedy", "Dante Alighieri");
 LObject.shelf[5].addBook("The Great Gatsby", "F. Scott Fitzgerald");
 LObject.shelf[4].addBook("Madame Bovary", "Gustave Flaubert");
 
-LObject.shelf[5].removeBook("The Divine Comedy");
-LObject.shelf[0].removeBook("The Di Comedy");
-// LObject.shelf[4].removeBook("Madame Bovary"); //, "Gustave Flaubert");
+LObject.shelf[5].removeBook("The Divine Comedy");//removed "The Divin Comedy"
+LObject.shelf[0].removeBook("The Di Comedy");  //no book called Di Comedy
 
-console.log(LObject.shelf[0].bookString);
-// console.log(SObject.book[0]);
-// console.log(LObject.shelf[3].bookArray);//String);
+// console.log(LObject.shelf[3].bookArray);//logs books on shelf[3]
 // console.log(LObject.shelf[3].bookString);
-console.log(SObject.book[4]);//************War and Peace book
-
-// console.log(LObject.shelf[5]);
-
+// console.log(SObject.book[4]);//************War and Peace book
 // console.log(SObject.book[5]);
 // console.log(library);  //*****
-console.log(library.string()); //****
-console.log(library.books());
+// console.log(library.string()); //logs shelfs
+console.log(library.books());  //logs books on shelfs
